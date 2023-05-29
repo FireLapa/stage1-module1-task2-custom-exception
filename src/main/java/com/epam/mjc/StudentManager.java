c441e2506230feeb04b6dc37cb47f37ca3babc8e
Lapa <lapabig@gmail.com>
2023-05-29 23:55:07 +0600

    Udated

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -6,16 +6,19 @@ public class StudentManager {
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+    Student x = Student.getValueOf(studentID);
+    if (x == null) {
+      throw new CantFindStudentException("Could not find student with ID " + studentID);
+    }
+    return x;
   }
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
-
     for (int i = 0; i < IDs.length; i++) {
       Student student = manager.find(IDs[i]);
       System.out.println("Student name " + student.getName());
-    }
 
+    }
   }
 }
